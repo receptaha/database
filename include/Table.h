e5ad13cef08b0f8d872babeaadcdee34cce9991b
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2026-04-30 22:48:01 +0300

    feat: implemented insert and serializeColumnData methods in Table class

--- include/Table.h
+++ include/Table.h
@@ -2,6 +2,7 @@
 #define TABLE_H
 #include <string>
 #include "Column.h"
+#include "Pager.h"
 using namespace std;
 
 class Table {
@@ -9,12 +10,18 @@ private:
     string name;
     vector<Column> columns;
     uint32_t rowSize;
+    uint32_t numRows;
+    Pager* pager;
     bool isColumnExists(const string& columnName) const;
+
 public:
-    Table(const string& name);
+    Table(const string& name, Pager* p);
     void addColumn(const string& columnName, DataType columnType);
 
     void printSchema() const;
+    void insert(const vector<Column>& columns, const vector<string>& values);
+
+    static bool serializeColumnData(const Column &column, const string &value, char *rowBuffer);
 };
 
 
--- src/Table.cpp
+++ src/Table.cpp
@@ -4,7 +4,10 @@
 
 #include "../include/Table.h"
 
-Table::Table(const string& name) : name(name) ,rowSize(0) {}
+#include "../include/Pager.h"
+
+Table::Table(const string& name, Pager* p) : name(name), rowSize(0), numRows(0), pager(p) {
+}
 
 void Table::addColumn(const string& columnName, DataType columnType) {
     if (this->isColumnExists(columnName)) {
@@ -22,11 +25,9 @@ void Table::addColumn(const string& columnName, DataType columnType) {
     this->rowSize += sizeOfColumn;
     this->columns.push_back(newColumn);
 
-    cout << "Sütun eklendi: (" << columnName << ")"
-        << " Boyut: " << sizeOfColumn
+    cout << "Column added: (" << columnName << ")"
+        << " Size: " << sizeOfColumn
         << " Column Offset: " << newColumn.offset << endl;
-
-
 }
 
 void Table::printSchema() const {
@@ -45,4 +46,88 @@ bool Table::isColumnExists(const string &columnName) const {
     }
     return false;
 }
+
+void Table::insert(const vector<Column> &inputColumns, const vector<string> &values) {
+    if (inputColumns.empty()) {
+        cerr << "INSERT ERROR: columns cannot be empty" << endl;
+        return;
+    }
+    if (values.empty()) {
+        cerr << "INSERT ERROR: values cannot be empty" << endl;
+        return;
+    }
+    if (inputColumns.size() != values.size()) {
+        cerr << "INSERT ERROR: columns count and values count must match" << endl;
+        return;
+    }
+
+    char* rowBuffer = new char[this->rowSize];
+
+    for (int i = 0; i < inputColumns.size(); i++) {
+        bool isMatching = false;
+        const Column& i_column = inputColumns[i];
+        for (int j = 0; j < this->columns.size(); j++) {
+            const Column& existColumn = this->columns[j];
+            if (i_column.name == existColumn.name) {
+                isMatching = true;
+                break;
+            }
+        }
+
+        if (!isMatching) {
+            cerr << "INSERT ERROR: Invalid column: '" << i_column.name << "'" << endl;
+            delete[] rowBuffer;
+            return;
+        }
+
+        bool success = serializeColumnData(i_column, values[i], rowBuffer);
+        if (!success) {
+            delete[] rowBuffer;
+            return;
+        }
+    }
+
+    uint32_t rowsPerPage = PAGE_SIZE / this->rowSize;
+    uint32_t pageNumber = this->numRows / rowsPerPage;
+    uint32_t offsetInPage = (this->numRows % rowsPerPage) * this->rowSize;
+    void* targetPage = this->pager->getPage(pageNumber);
+    memcpy((char*) targetPage + offsetInPage, rowBuffer, this->rowSize);
+
+    this->numRows++;
+    this->pager->flush(pageNumber);
+    delete[] rowBuffer;
+
+    cout << "SUCCESS: Records has been writen to disk! -> Page: " << pageNumber << ", Total offset: " << numRows * rowSize << endl;
+}
+
+bool Table::serializeColumnData(const Column& column, const string& value, char* rowBuffer) {
+
+    if (column.type == DataType::INT) {
+        try {
+            int intValue = stoi(value);
+
+            memcpy(rowBuffer + column.offset, &intValue, column.size);
+            return true;
+        } catch (...) {
+            cerr << "INSERT ERROR: Type mismatch! '" << value << "' is not a valid integer for column '" << column.name << "'" << endl;
+            return false;
+        }
+    }
+
+    if (column.type == DataType::VARCHAR_32 || column.type == DataType::VARCHAR_255) {
+
+        if (value.size() > column.size) {
+            cerr << "INSERT ERROR: Size overflow: '" << value << "' is too long for column '" << column.name << "'" << endl;
+            return false;
+        }
+
+        memset(rowBuffer + column.offset, 0, column.size);
+        memcpy(rowBuffer + column.offset, value.c_str(), value.size());
+        return true;
+    }
+
+    return false;
+}
+
+
 
