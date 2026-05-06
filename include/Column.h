1e4f1be9b7869cda6b20c78910ccbc7ee74663bd
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2026-05-07 00:01:06 +0300

    feat: created SUPPORTED_TYPES const unordered_map and SUPPORTED_CONSTRAINTS const unordered_set

--- include/Column.h
+++ include/Column.h
@@ -2,6 +2,7 @@
 #define COLUMN_H
 #include <string>
 #include "DataType.h"
+#include <unordered_set>
 using namespace std;
 
 struct Column {
@@ -9,6 +10,17 @@ struct Column {
     DataType type;
     uint32_t size;
     uint32_t offset;
+    vector<string> constraints;
+
+    inline static const unordered_map<string, DataType> SUPPORTED_TYPES = {
+        {"INT", DataType::INT},
+        {"VARCHAR", DataType::VARCHAR},
+        {"BOOL", DataType::BOOLEAN},
+    };
+
+    inline static const unordered_set<string> SUPPORTED_CONSTRAINTS = {
+        "PRIMARY KEY", "NOT NULL", "AUTO INCREMENT", "UNIQUE"
+    };
 };
 
 #endif
